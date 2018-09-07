7c8ff447b847684d12c9ea38cf0815392fa68212
unknown <ju33un@gmail.com>
2018-09-08 04:45:05 +0900

    [fix] LinkedList

--- LinearStructure/LinkedList/List.cpp
+++ LinearStructure/LinkedList/List.cpp
@@ -2,9 +2,7 @@
 
 List::List(int p_iData)
 {
-	m_pHeadNode = new LinkedNode();
-
-	m_pHeadNode->SetData(p_iData);
+	m_pHeadNode = new LinkedNode(p_iData);
 }
 
 
@@ -22,26 +20,61 @@ void List::Add(int p_iData)
 {
 	if ( m_pHeadNode != nullptr )
 	{
-		LinkedNode* pNewNode = new LinkedNode();
+		LinkedNode* pNewNode = new LinkedNode(p_iData);
 
-		// List size를 확인하고 마지막 노드에 추가
+		/// TEST_CODE : FindNode()함수 완성 후 다시 수정 예정
+		// List 마지막 노드에 추가
+		LinkedNode* pFinalNode = m_pHeadNode;
+		while ( pFinalNode->GetNextNode() != nullptr )
+		{
+			pFinalNode = pFinalNode->GetNextNode();
+		}
 
-
+		pFinalNode->SetNextNode(pNewNode);
+		///
 	}
 	else // Head가 없을 때
 	{
-		m_pHeadNode = new LinkedNode();
-
-		m_pHeadNode->SetData(p_iData);
+		m_pHeadNode = new LinkedNode(p_iData);
 	}
 }
 
 void List::Add(int p_iIndex, int p_iData)
 {
+	// 특정 index와 전체 리스트 수와 검사하는 코드 추가
+
+	/// 상황 : Index가 0 1 2 3 4인 총 5개 노드를 가지고 있다고 가정할 때
+	///        Index 3에 추가하고 싶을 때
+	/// SOLUTION
+	/// 1. Index 2의 NextNode(Index 3의 노드 주소)를 저장해서 새로운 노드의 NextNode로 지정
+	/// 2. Index 2의 NextNode를 새로운 노드로 지정
 
 }
 
 unsigned int List::GetListSize()
 {
-	return m_iListSize;
+	//return m_iListSize;
+}
+
+/// TEST_CODE : Final이나 특정 index 노드의 주소를 찾기 위함 (기본 -1 : Final / 특정 노드 : index)
+LinkedNode* List::FindNode(int p_iIndex)
+{
+	if ( m_pHeadNode != nullptr )
+	{
+		int iNowIndex = 0;
+		LinkedNode* pFindNode = m_pHeadNode;
+		
+		/// 만약 iNowIndex가 -1일 경우는 마지막까지
+		//while ( pFindNode->GetNextNode() != nullptr )
+		//{
+		//	pFindNode = pFindNode->GetNextNode();
+		//}
+		/// 아닌 경우에는 일단 iNowIndex와 p_iIndex 비교 
+		/// 근데 이 때 그 Index가 실제 존재하는 건지는 반드시 검사하는 코드가 존재해야 함
+
+
+
+		return pFindNode;
+	}
+	return nullptr;
 }
--- LinearStructure/LinkedList/List.h
+++ LinearStructure/LinkedList/List.h
@@ -14,7 +14,12 @@ public:
 
 	unsigned int GetListSize();
 
+	/// TEST_CODE
+	LinkedNode* FindNode(int p_iIndex = -1);
+
 private:
 	LinkedNode* m_pHeadNode = nullptr;
-	unsigned int m_iListSize = 0;
+	//unsigned int m_iListSize = 0;
 };
+
+/// 기능 완성 후 스마트 포인터로 수정!!!
